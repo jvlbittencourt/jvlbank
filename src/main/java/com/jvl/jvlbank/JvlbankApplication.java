49a1e344ff6ac7e257b255b15068e87f26f7265b
jbittencourt <joao_bittencourt@id.uff.br>
2025-04-08 17:33:48 -0300

    first commit: Initial Spring Boot setup and PostgreSQL config.

---
+++ src/main/java/com/jvl/jvlbank/JvlbankApplication.java
@@ -0,0 +1,13 @@
+package com.jvl.jvlbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvlbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvlbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jvl/jvlbank/JvlbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvl.jvlbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvlbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
