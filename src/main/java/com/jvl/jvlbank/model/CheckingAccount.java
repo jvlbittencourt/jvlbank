300ed77b5a9e602a8a8ce3d3a8b0da6d982d1d0d
jbittencourt <joao_bittencourt@id.uff.br>
2025-04-09 16:29:10 -0300

    refactoring: Account, CheckingAccount and SavingAccount

--- src/main/java/com/jvl/jvlbank/model/CheckingAccount.java
+++ src/main/java/com/jvl/jvlbank/model/CheckingAccount.java
@@ -1,5 +1,29 @@
 package com.jvl.jvlbank.model;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Entity
+@DiscriminatorValue("CHEKCING")
 public class CheckingAccount extends Account{
 
+    private BigDecimal overdraftLimit;
+    private BigDecimal maintenanceFee;
+
+    @Override
+    public boolean canWithdraw(BigDecimal amount){
+        var limit = getBalance().add(overdraftLimit);
+
+        if (getBalance().compareTo(amount) >= 0 || limit.compareTo(amount) >= 0)
+            return true;
+        return false;
+    }
 }
--- src/main/java/com/jvl/jvlbank/model/SavingAccount.java
+++ src/main/java/com/jvl/jvlbank/model/SavingAccount.java
@@ -1,5 +1,27 @@
 package com.jvl.jvlbank.model;
 
-public class SavingAccount extends Account{
+import java.math.BigDecimal;
 
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Entity
+@DiscriminatorValue("SAVING")
+public class SavingAccount extends Account{
+    private BigDecimal interestRate;
+
+    @Override
+    public boolean canWithdraw(BigDecimal amount){
+
+        if (getBalance().compareTo(amount) >= 0)
+            return true;
+        return false;
+    }
 }
